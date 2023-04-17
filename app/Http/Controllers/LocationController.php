83a163d9cf1051b13bfc6bfa26f3713ab52f7fc9
Kyle <kyle.mcluckie@gmail.com>
2023-04-17 11:25:02 +1000

    Add location controller and route
    Controller to return all Location models at this stage

---
+++ app/Http/Controllers/LocationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Location;
+use Illuminate\Http\Request;
+
+class LocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Location::all();
+    }
+}
